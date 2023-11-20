b60fb468c00a089b90833fc7e29ea8ab188ff052
Euclid <106529460+Aethereux@users.noreply.github.com>
2023-11-20 15:10:13 +0800

    Update Factorial Exercise.cpp

--- For Loops Exercises/Factorial Exercise.cpp
+++ For Loops Exercises/Factorial Exercise.cpp
@@ -3,16 +3,19 @@
 using namespace std;
 
 int main() {
-	int n, number = 1;
-
-	cout << "Enter the number: ";
-	cin >> n;
-
-	// formula for factorial is n! = n * n - 1
-	for(int i = 1; i <= n; i++) {
-		number *= i;
+	int n, number = 1, count = 1;
+	// The instruction said that it should repeat 3 times.
+	while (count <= 3) {
+		cout << "Enter the number: ";
+		cin >> n;
+	
+		// formula for factorial is n! = n * n - 1
+		for(int i = 1; i <= n; i++) {
+			number *= i;
+		}
+		
+		cout << "The factorial of " << n << " is " << number << endl;
+		number = 1; // resets to 1 to avoid multiple results
+		count++;
 	}
-	
-	cout << "The factorial of " << n << " is " << number;
-	
 }
