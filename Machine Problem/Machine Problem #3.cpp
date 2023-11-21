04db937ffcb2523fac0a720a8997548d481f73be
Euclid <106529460+Aethereux@users.noreply.github.com>
2023-11-21 20:35:11 +0800

    Update Machine Problem #3.cpp

--- Machine Problem/Machine Problem #3.cpp
+++ Machine Problem/Machine Problem #3.cpp
@@ -3,21 +3,25 @@
 using namespace std;
 
 int main() {
-	int input, digit;
-
-	cout << "Enter an integer greater than 0: ";
-	cin >> input;
+		int input, digit;
+		
+	do {
+		cout << "\nEnter an integer greater than 0: ";
+		cin >> input;
+		
+		if(input <= 0) {
+			cout << "You entered an invalid number! Program terminating!";
+			return 0;
+		}
 	
-	if(input <= 0) {
-	cout << "You entered an invalid number! Program terminating!";
-	return 0;
+		while (input != 0) {
+			input = input / 10;
+			digit++;
 	}
 	
-	while (input != 0) {
-		input = input / 10;
-		digit++;
-	}
-
-	cout << "The number has " << digit << " digits.";
-
+		cout << "The number has " << digit << " digits.";
+	   	digit = 0; // resets to 0
+	} while (input <= 0);
+	
+	
 }
